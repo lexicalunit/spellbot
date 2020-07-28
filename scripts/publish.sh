490b4016cba170f3438752bb7d339c74892b2aca
Amy Troschinetz <amy@lexicalunit.com>
2020-07-27 19:23:48 -0700

    wip

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -27,6 +27,7 @@ if [[ "$KIND" != "major" && "$KIND" != "minor" && "$KIND" != "patch" ]]; then
     usage
 fi
 
+REPO_ROOT="$(git rev-parse --show-toplevel)"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 if [[ $BRANCH != "master" ]]; then
     echo "error: you must be on the master branch to publish" 1>&2
@@ -47,6 +48,35 @@ run "tox"
 
 # fetch the version from pyproject.toml
 VERSION="$(grep "^version" < pyproject.toml | cut -d= -f2 | sed 's/"//g;s/ //g;s/^/v/;')"
+
+# promote unreleased changes in the changelog
+OLD_CHANGELOG="$REPO_ROOT/CHANGELOG.md"
+NEW_CHANGELOG="$REPO_ROOT/CHANGELOG-$VERSION.md"
+cat >"$NEW_CHANGELOG" <<EOF
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+EOF
+
+BODY_STARTED="false"
+while IFS= read -r line
+do
+    if [[ "$BODY_STARTED" == "false" ]]; then
+        if [[ $line == *"[Unreleased]"* ]]; then
+            BODY_STARTED="true"
+            echo "## [$VERSION](https://github.com/lexicalunit/spellbot/releases/tag/$VERSION) - $(date +'%Y-%m-%d')" >> "$NEW_CHANGELOG"
+        fi
+    else
+        echo "$line" >> "$NEW_CHANGELOG"
+    fi
+done <"$OLD_CHANGELOG"
+run "mv '$NEW_CHANGELOG' '$OLD_CHANGELOG'"
 
 # build the release
 run "poetry build"
