f43424f99fae961537b9b59c4ce9e6e08f217b25
Amy <amy@lexicalunit.com>
2020-07-29 19:09:00 -0700

    Workaround cache issues

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Fixed
+
+- Workaround over-eager caching of embed thumbnail images by discord.
+
 ## [v3.13.8](https://github.com/lexicalunit/spellbot/releases/tag/v3.13.8) - 2020-07-29
 
 ### Added
--- src/spellbot/constants.py
+++ src/spellbot/constants.py
@@ -1,5 +1,13 @@
-# Application Settings
+from datetime import datetime
+
 ADMIN_ROLE = "SpellBot Admin"
 CREATE_ENDPOINT = "https://us-central1-magic-night-30324.cloudfunctions.net/createGame"
-THUMB_URL = "https://raw.githubusercontent.com/lexicalunit/spellbot/master/spellbot.png"
+THUMB_URL = (
+    "https://raw.githubusercontent.com/lexicalunit/spellbot/master/spellbot.png"
+    f"?{datetime.today().strftime('%Y-%m-%d')}"  # workaround over-eager caching
+)
 DEFAULT_GAME_SIZE = 4
+INVITE_LINK = (
+    "https://discordapp.com/api/oauth2/authorize"
+    "?client_id=725510263251402832&permissions=92224&scope=bot"
+)
