a592c17b2ba40cd7160ce43d0d786a04cf88a944
Blade Barringer <blade@crookedneighbor.com>
2020-08-01 01:31:01 +0000

    Pull mock users into separate file

--- tests/test_spellbot.py
+++ tests/test_spellbot.py
@@ -17,11 +17,26 @@ from mocks.discord import (  # type: ignore
     MockAttachment,
     MockChannel,
     MockDM,
-    MockMember,
     MockMessage,
     MockPayload,
-    MockRole,
     MockTextChannel,
+)
+from mocks.users import (  # type: ignore
+    ADAM,
+    ADMIN,
+    ADMIN_ROLE,
+    ALL_USERS,
+    AMY,
+    BOT,
+    BUDDY,
+    DUDE,
+    FRIEND,
+    GUY,
+    JACOB,
+    JR,
+    PUNK,
+    SERVER_MEMBERS,
+    TOM,
 )
 
 import spellbot
@@ -59,8 +74,6 @@ class MockDiscordClient:
 
 CLIENT_TOKEN = "my-token"
 CLIENT_AUTH = "my-auth"
-CLIENT_USER = "ADMIN"
-CLIENT_USER_ID = 82226367030108160
 
 TST_ROOT = Path(__file__).resolve().parent
 FIXTURES_ROOT = TST_ROOT / "fixtures"
@@ -68,29 +81,6 @@ REPO_ROOT = TST_ROOT.parent
 SRC_ROOT = REPO_ROOT / "src"
 
 SRC_DIRS = [REPO_ROOT / "tests", SRC_ROOT / "spellbot", REPO_ROOT / "scripts"]
-
-ADMIN_ROLE = MockRole("SpellBot Admin")
-PLAYER_ROLE = MockRole("Player Player")
-
-ADMIN = MockMember(CLIENT_USER, CLIENT_USER_ID, roles=[ADMIN_ROLE], admin=True)
-FRIEND = MockMember("friend", 82169952898900001, roles=[PLAYER_ROLE])
-BUDDY = MockMember("buddy", 82942320688700002, roles=[ADMIN_ROLE, PLAYER_ROLE])
-GUY = MockMember("guy", 82988021019800003)
-DUDE = MockMember("dude", 82988761019800004, roles=[ADMIN_ROLE])
-
-JR = MockMember("J.R.", 72988021019800005)
-ADAM = MockMember("Adam", 62988021019800006)
-TOM = MockMember("Tom", 62988021019800016)
-AMY = MockMember("Amy", 52988021019800007)
-JACOB = MockMember("Jacob", 42988021019800008)
-
-PUNK = MockMember("punk", 119678027792600009)  # for a memeber that's not in our channel
-BOT = MockMember("robot", 82169567890900010)
-BOT.bot = True
-ADMIN.bot = True
-
-SERVER_MEMBERS = [FRIEND, BUDDY, GUY, DUDE, ADMIN, JR, ADAM, TOM, AMY, JACOB]
-ALL_USERS = []  # users that are on the server, setup in client fixture
 
 S_SPY = Mock(wraps=spellbot.s)
 
