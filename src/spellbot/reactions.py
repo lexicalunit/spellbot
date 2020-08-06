27b48fa30ff0d3f3d93e4393d5ad81becf625edf
Blade Barringer <blade@crookedneighbor.com>
2020-08-06 03:46:05 +0000

    Post to channel when bot does not have reaction permissions
    closes #120

--- src/spellbot/reactions.py
+++ src/spellbot/reactions.py
@@ -2,6 +2,8 @@ import logging
 
 import discord
 
+from spellbot.assets import s
+
 logger = logging.getLogger(__name__)
 
 
@@ -10,9 +12,10 @@ async def safe_remove_reaction(
 ) -> None:  # pragma: no cover
     try:
         await message.remove_reaction(emoji, user)
+    except discord.errors.Forbidden:
+        await message.channel.send(s("reaction_permissions_required"))
     except (
         discord.errors.HTTPException,
-        discord.errors.Forbidden,
         discord.errors.NotFound,
         discord.errors.InvalidArgument,
     ) as e:
@@ -22,5 +25,7 @@ async def safe_remove_reaction(
 async def safe_clear_reactions(message: discord.Message) -> None:  # pragma: no cover
     try:
         await message.clear_reactions()
-    except (discord.errors.HTTPException, discord.errors.Forbidden) as e:
+    except discord.errors.Forbidden:
+        await message.channel.send(s("reaction_permissions_required"))
+    except discord.errors.HTTPException as e:
         logger.exception("warning: discord: could not clear reactions", e)
