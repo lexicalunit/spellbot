0d0ad117145f4548639756ab382620e863794faf
Amy <amy@lexicalunit.com>
2020-08-07 08:54:53 -0700

    Adds full test coverage for reactions module

--- tests/test_reactions.py
+++ tests/test_reactions.py
@@ -1,3 +1,4 @@
+from typing import cast
 from unittest.mock import Mock, patch
 
 import discord
@@ -16,7 +17,9 @@ class TestReactions:
         with patch.object(
             message, "remove_reaction", wraps=message.remove_reaction
         ) as fake_message:
-            await safe_remove_reaction(message, "emoji", FRIEND)
+            await safe_remove_reaction(
+                cast(discord.Message, message), "emoji", cast(discord.User, FRIEND)
+            )
 
             fake_message.assert_called_with("emoji", FRIEND)
 
@@ -35,21 +38,35 @@ class TestReactions:
             with patch.object(
                 message.channel, "send", wraps=message.channel.send
             ) as fake_channel_send:
-                await safe_remove_reaction(message, "emoji", FRIEND)
+                await safe_remove_reaction(
+                    cast(discord.Message, message), "emoji", cast(discord.User, FRIEND)
+                )
 
                 fake_channel_send.assert_called_with(
-                    (
-                        "I do not have permission to adjust "
-                        "reactions. A server admin will need to adjust my permissions."
-                    )
+                    "I do not have permission to adjust "
+                    "reactions. A server admin will need to adjust my permissions."
                 )
+
+    async def test_remove_reaction_when_http_error_is_thrown(self, caplog):
+        message = MockDiscordMessage()
+        response = Mock()
+        response.status = 403
+        with patch.object(message, "remove_reaction", wraps=message.remove_reaction) as m:
+            m.side_effect = discord.errors.HTTPException(response, "BAD REQUEST")
+            with patch.object(message.channel, "send", wraps=message.channel.send):
+                await safe_remove_reaction(
+                    cast(discord.Message, message), "emoji", cast(discord.User, FRIEND)
+                )
+
+                assert "warning: discord: could not remove reaction" in caplog.text
+                assert "BAD REQUEST" in caplog.text
 
     async def test_clear_reactions_clears_reactions_from_message(self):
         message = MockDiscordMessage()
         with patch.object(
             message, "clear_reactions", wraps=message.clear_reactions
         ) as fake_message:
-            await safe_clear_reactions(message)
+            await safe_clear_reactions(cast(discord.Message, message))
 
             fake_message.assert_called()
 
@@ -68,11 +85,21 @@ class TestReactions:
             with patch.object(
                 message.channel, "send", wraps=message.channel.send
             ) as fake_channel_send:
-                await safe_clear_reactions(message)
+                await safe_clear_reactions(cast(discord.Message, message))
 
                 fake_channel_send.assert_called_with(
-                    (
-                        "I do not have permission to adjust "
-                        "reactions. A server admin will need to adjust my permissions."
-                    )
+                    "I do not have permission to adjust "
+                    "reactions. A server admin will need to adjust my permissions."
                 )
+
+    async def test_clear_reactions_when_http_error_is_thrown(self, caplog):
+        message = MockDiscordMessage()
+        fake_response = Mock()
+        fake_response.status = 400
+        with patch.object(message, "clear_reactions", wraps=message.clear_reactions) as m:
+            m.side_effect = discord.errors.HTTPException(fake_response, "BAD REQUEST")
+            with patch.object(message.channel, "send", wraps=message.channel.send):
+                await safe_clear_reactions(cast(discord.Message, message))
+
+                assert "warning: discord: could not clear reactions" in caplog.text
+                assert "BAD REQUEST" in caplog.text
