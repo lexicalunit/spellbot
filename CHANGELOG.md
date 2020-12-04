11ca4d2c44ad12a75c0904f70f8db0223e473765
Amy Troschinetz <amy@lexicalunit.com>
2020-12-03 23:32:11 -0800

    Update to latest from beautiful-jekyll

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ### Changed
 
 - Moves top.gg widget to navbar.
+- Update to latest from beautiful-jekyll.
 
 ## [v5.10.0](https://github.com/lexicalunit/spellbot/releases/tag/v5.10.0) - 2020-11-23
 
---
+++ docs/assets/js/spellbot.js
@@ -0,0 +1,7 @@
+var SpellBotJS = {
+  init : function() {
+    $('#main-navbar .navbar-nav').append('<li class="nav-item"><a class="nav-link" href="https://top.gg/bot/725510263251402832"><img src="https://top.gg/api/widget/status/725510263251402832.svg?noavatar=true" alt="SpellBot" /></a></li>');
+  },
+};
+
+document.addEventListener('DOMContentLoaded', SpellBotJS.init);
