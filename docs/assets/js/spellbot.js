83d94c414c7927bc05605c367c5d6fd90a76283b
Amy <amy@lexicalunit.com>
2021-10-19 14:12:53 -0700

    Remove top.gg integration

--- docs/assets/js/spellbot.js
+++
@@ -1,9 +0,0 @@
-var SpellBotJS = {
-  init : function() {
-    const link = $('#main-navbar .navbar-nav .nav-item:last-child .nav-link')
-    link.href = 'https://top.gg/bot/725510263251402832';
-    link.html('<img src="https://top.gg/api/widget/status/725510263251402832.svg?noavatar=true" alt="" width="123" height="20" />')
-  },
-};
-
-document.addEventListener('DOMContentLoaded', SpellBotJS.init);
