af4aa2b121f662309233524f659134d57d0ed70c
Amy <amy@lexicalunit.com>
2021-11-05 15:57:30 -0700

    Adds some pyright config, remove mypy config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+from _pytest.config import Config
+from pytest import Item, Session
+
 # Make all fixtures available to all tests
 from .fixtures import *  # noqa
 
@@ -7,15 +10,20 @@ SUITE_ORDER = [
 ]
 
 
-def pytest_collection_modifyitems(session, config, items):
-    def order(f):
-        suite = f.cls.__name__
+def pytest_collection_modifyitems(session: Session, config: Config, items: list[Item]):
+    def order(item: Item):
+        if not item:
+            return 0
+        cls = getattr(item, "cls", None)
+        if not cls:
+            return 0
+        suite = cls.__name__
         return SUITE_ORDER.index(suite) + 1 if suite in SUITE_ORDER else 0
 
     items.sort(key=order)
 
 
-def pytest_configure(config):
+def pytest_configure(config: Config):
     config.addinivalue_line(
         "markers",
         "nosession: mark test to run without a database session",
