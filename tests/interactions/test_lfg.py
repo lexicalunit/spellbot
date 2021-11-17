3ec7d4bd84c9eedbf22a5aaf63eb5d7298b1325a
Amy Troschinetz <amy@lexicalunit.com>
2021-11-17 15:14:27 -0800

    Fixing some codefactor issues

--- tests/interactions/test_lfg.py
+++ tests/interactions/test_lfg.py
@@ -1,26 +1,17 @@
 from unittest.mock import AsyncMock
 
 import pytest
-from discord_slash.context import InteractionContext
 
-from spellbot import SpellBot
 from spellbot.database import DatabaseSession
 from spellbot.interactions import LookingForGameInteraction, lfg_interaction
 from spellbot.models import User
-from tests.fixtures import Factories
+from tests.mixins import InteractionContextMixin
 from tests.mocks import build_author
-
-# TODO: Rewrite these tests using mock_operations().
 
 
 @pytest.mark.asyncio
-class TestInteractionLookingForGame:
-    async def test_ensure_users_happy_path(
-        self,
-        bot: SpellBot,
-        ctx: InteractionContext,
-        monkeypatch,
-    ):
+class TestInteractionLookingForGame(InteractionContextMixin):
+    async def test_ensure_users_happy_path(self, monkeypatch):
         user_list = [build_author(xid) for xid in range(1, 4)]
         user_dict = {user.id: user for user in user_list}
         user_xids = list(user_dict.keys())
@@ -31,20 +22,15 @@ class TestInteractionLookingForGame:
         mock_sfu = AsyncMock(side_effect=fetch_user)
         monkeypatch.setattr(lfg_interaction, "safe_fetch_user", mock_sfu)
 
-        async with LookingForGameInteraction.create(bot, ctx) as interaction:
+        async with LookingForGameInteraction.create(self.bot, self.ctx) as interaction:
             created = await interaction.ensure_users_exist(user_xids)
             assert set(created) == set(
-                user.id for user in user_list if user.id != ctx.author_id
+                user.id for user in user_list if user.id != self.ctx.author_id
             )
 
         assert DatabaseSession.query(User).count() == 3
 
-    async def test_ensure_users_without_exclude_self(
-        self,
-        bot: SpellBot,
-        ctx: InteractionContext,
-        monkeypatch,
-    ):
+    async def test_ensure_users_without_exclude_self(self, monkeypatch):
         user_list = [build_author(xid) for xid in range(1, 4)]
         user_dict = {user.id: user for user in user_list}
         user_xids = list(user_dict.keys())
@@ -55,18 +41,13 @@ class TestInteractionLookingForGame:
         mock_sfu = AsyncMock(side_effect=fetch_user)
         monkeypatch.setattr(lfg_interaction, "safe_fetch_user", mock_sfu)
 
-        async with LookingForGameInteraction.create(bot, ctx) as interaction:
+        async with LookingForGameInteraction.create(self.bot, self.ctx) as interaction:
             created = await interaction.ensure_users_exist(user_xids, exclude_self=False)
             assert set(created) == set(user_xids)
 
         assert DatabaseSession.query(User).count() == 3
 
-    async def test_ensure_users_when_fetch_user_fails(
-        self,
-        bot: SpellBot,
-        ctx: InteractionContext,
-        monkeypatch,
-    ):
+    async def test_ensure_users_when_fetch_user_fails(self, monkeypatch):
         user_list = [build_author(xid) for xid in range(1, 4)]
         user_dict = {user.id: user for user in user_list}
         user_xids = list(user_dict.keys())
@@ -79,7 +60,7 @@ class TestInteractionLookingForGame:
         mock_sfu = AsyncMock(side_effect=fetch_user)
         monkeypatch.setattr(lfg_interaction, "safe_fetch_user", mock_sfu)
 
-        async with LookingForGameInteraction.create(bot, ctx) as interaction:
+        async with LookingForGameInteraction.create(self.bot, self.ctx) as interaction:
             created = await interaction.ensure_users_exist(user_xids, exclude_self=False)
             assert set(created) == set(
                 user.id for user in user_list if user.id != user_list[-1].id
@@ -87,18 +68,12 @@ class TestInteractionLookingForGame:
 
         assert DatabaseSession.query(User).count() == 2
 
-    async def test_ensure_users_when_user_banned(
-        self,
-        bot: SpellBot,
-        ctx: InteractionContext,
-        factories: Factories,
-        monkeypatch,
-    ):
+    async def test_ensure_users_when_user_banned(self, monkeypatch):
         user_list = [build_author(xid) for xid in range(1, 4)]
         user_dict = {user.id: user for user in user_list}
         user_xids = list(user_dict.keys())
         banned_user_xid = user_list[-1].id
-        factories.user.create(xid=banned_user_xid, banned=True)
+        self.factories.user.create(xid=banned_user_xid, banned=True)
 
         async def fetch_user(_, xid):
             return user_dict[xid]
@@ -106,7 +81,7 @@ class TestInteractionLookingForGame:
         mock_sfu = AsyncMock(side_effect=fetch_user)
         monkeypatch.setattr(lfg_interaction, "safe_fetch_user", mock_sfu)
 
-        async with LookingForGameInteraction.create(bot, ctx) as interaction:
+        async with LookingForGameInteraction.create(self.bot, self.ctx) as interaction:
             created = await interaction.ensure_users_exist(user_xids, exclude_self=False)
             assert set(created) == set(
                 user.id for user in user_list if user.id != banned_user_xid
