f70799136e19fc6b5a3a9c6368532538d5c7f066
Amy Troschinetz <amy@lexicalunit.com>
2021-12-16 14:46:58 -0800

    Refactor leave command and button

--- src/spellbot/interactions/leave_interaction.py
+++ src/spellbot/interactions/leave_interaction.py
@@ -18,7 +18,32 @@ logger = logging.getLogger(__name__)
 
 class LeaveInteraction(BaseInteraction):
     @tracer.wrap()
-    async def report_success(self):
+    async def _handle_click(self):
+        assert self.ctx
+        ctx: ComponentContext = cast(ComponentContext, self.ctx)
+
+        if not (game_id := await self.services.users.current_game_id()):
+            return ctx.defer(ignore=True)
+
+        found = await self.services.games.select(game_id)
+        assert found
+
+        game_data = await self.services.games.to_dict()
+        if not (message_xid := game_data["message_xid"]):
+            return ctx.defer(ignore=True)
+
+        if ctx.origin_message_id != message_xid:
+            return await safe_send_channel(
+                ctx,
+                "You're not in that game. Use the /leave command to leave a game.",
+                hidden=True,
+            )
+
+        await self.services.users.leave_game()
+        embed = await self.services.games.to_embed()
+        await safe_update_embed_origin(ctx, embed=embed)
+
+    async def _removed(self):
         assert self.ctx
         await safe_send_channel(
             self.ctx,
@@ -27,34 +52,34 @@ class LeaveInteraction(BaseInteraction):
         )
 
     @tracer.wrap()
-    async def execute(self, origin: bool = False):
+    async def _handle_command(self):
+        assert self.ctx
+
         if not (game_id := await self.services.users.current_game_id()):
-            return await self.report_success()
+            return await self._removed()
 
-        await self.services.games.select(game_id)
-
-        bot = self.bot
-        game_data = await self.services.games.to_dict()
-        channel_xid = game_data["channel_xid"]
-        guild_xid = game_data["guild_xid"]
+        found = await self.services.games.select(game_id)
+        assert found
 
         await self.services.users.leave_game()
 
-        if not (channel := await safe_fetch_text_channel(bot, guild_xid, channel_xid)):
-            return await self.report_success()
+        game_data = await self.services.games.to_dict()
+        chan_xid = game_data["channel_xid"]
+        guild_xid = game_data["guild_xid"]
+
+        if not (channel := await safe_fetch_text_channel(self.bot, guild_xid, chan_xid)):
+            return await self._removed()
         if not (message_xid := game_data["message_xid"]):
-            return await self.report_success()
+            return await self._removed()
         if not (message := safe_get_partial_message(channel, guild_xid, message_xid)):
-            return await self.report_success()
+            return await self._removed()
 
         embed = await self.services.games.to_embed()
-
-        if origin:
-            # self.ctx should be a ComponentContext from a button click
-            ctx: ComponentContext = cast(ComponentContext, self.ctx)
-            if ctx.origin_message_id == message_xid:
-                return await safe_update_embed_origin(ctx, embed=embed)
-            return await self.report_success()
-
         await safe_update_embed(message, embed=embed)
-        await self.report_success()
+        await self._removed()
+
+    @tracer.wrap()
+    async def execute(self, origin: bool = False):
+        if origin:
+            return await self._handle_click()
+        return await self._handle_command()
