1f3de03822296b875cd3f73ecedceb45b7d52224
Amy Troschinetz <amy@lexicalunit.com>
2022-03-05 13:16:57 -0800

    Adding some logging around supervisor/docker startup

--- scripts/start-spellbot.sh
+++ scripts/start-spellbot.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "starting spellbot..."
+
 if [[ -n $DD_API_KEY ]] && [[ -n $DD_APP_KEY ]]; then
+    echo "running with ddtrace enabled..."
     DD_VERSION="$(spellbot --version)"
     export DD_VERSION
     ddtrace-run spellbot -s
 else
+    echo "running without ddtrace..."
     spellbot -s
 fi
