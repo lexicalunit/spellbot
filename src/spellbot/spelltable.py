a4ae92375c37ac9239a1d53c96787054d9b818c8
lexicalunit <lexicalunit@lexicalunit.com>
2022-04-28 10:50:23 -0700

    Workaround json issue in spelltable api

--- src/spellbot/spelltable.py
+++ src/spellbot/spelltable.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from typing import Optional
 
@@ -20,20 +21,27 @@ async def generate_link() -> Optional[str]:
         "key": settings.SPELLTABLE_AUTH_KEY,
     }
 
+    data: Optional[dict] = None
+    raw_data: Optional[bytes] = None
     try:
         async with RetryClient(
             raise_for_status=False,
             retry_options=ExponentialRetry(attempts=5),
         ) as client:
             async with client.post(settings.SPELLTABLE_CREATE, headers=headers) as resp:
-                data = await resp.json()
-                if "gameUrl" not in data:
+                # Rather than use `resp.json()`, which respects minetype, let's just
+                # grab the data and try to decode it ourselves.
+                # https://github.com/inyutin/aiohttp_retry/issues/55
+                raw_data = await resp.read()
+                data = json.loads(raw_data)
+                if not data or "gameUrl" not in data:
                     logger.warning(
                         "warning: gameUrl missing from SpellTable API response (%s): %s",
                         resp.status,
                         data,
                     )
                     return None
+                assert data is not None
                 returned_url = str(data["gameUrl"])
                 wizards_url = returned_url.replace(
                     "www.spelltable.com",
@@ -42,5 +50,21 @@ async def generate_link() -> Optional[str]:
                 return wizards_url
     except ClientError as ex:
         add_span_error(ex)
-        logger.warning("warning: SpellTable API failure: %s", ex, exc_info=True)
+        logger.warning(
+            "warning: SpellTable API failure: %s, data: %s, raw: %s",
+            ex,
+            data,
+            raw_data,
+            exc_info=True,
+        )
+        return None
+    except Exception as ex:
+        add_span_error(ex)
+        logger.error(
+            "error: unexpected exception: %s, data: %s, raw: %s",
+            ex,
+            data,
+            raw_data,
+            exc_info=True,
+        )
         return None
