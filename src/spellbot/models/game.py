192ba4c5420677e7971cbdffe4dde8023c121013
lexicalunit <lexicalunit@lexicalunit.com>
2022-11-21 23:38:11 -0800

    Rename battle cruiser to one word

--- src/spellbot/models/game.py
+++ src/spellbot/models/game.py
@@ -59,7 +59,7 @@ class GameFormat(Enum):
     EDH_HIGH = FormatDetails(players=4)
     EDH_MID = FormatDetails(players=4)
     EDH_LOW = FormatDetails(players=4)
-    EDH_BATTLE_CRUISER = FormatDetails(players=4)
+    EDH_BATTLECRUISER = FormatDetails(players=4)
 
 
 class Game(Base):
