365288659e249ee216bb48d57dfece0298339545
lexicalunit <lexicalunit@lexicalunit.com>
2022-12-06 16:03:58 -0800

    Rework the old utils tests

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -7,6 +7,7 @@ import pytest
 from discord import app_commands
 from discord.ext.commands.bot import AutoShardedBot
 from spellbot import SpellBot, client
+from spellbot.database import DatabaseSession
 from spellbot.errors import (
     AdminOnlyError,
     GuildOnlyError,
@@ -14,7 +15,10 @@ from spellbot.errors import (
     UserUnverifiedError,
     UserVerifiedError,
 )
+from spellbot.models import Channel, Guild, Verify
 from spellbot.utils import handle_interaction_errors
+
+from .mixins import BaseMixin
 
 
 @pytest.mark.asyncio
@@ -130,194 +134,201 @@ class TestSpellBot:
         dpy_message.reply.assert_not_called()
 
 
-# TODO:
-# @pytest.mark.asyncio
-# class TestSpellBotHandleVerification(BaseMixin):
-#     async def test_missing_author_id(self, monkeypatch):
-#         message = MagicMock()
-#         message.guild = MagicMock()
-#         message.guild.id = 2
-#         message.channel = MagicMock()
-#         message.channel.type = discord.ChannelType.text
-#         message.flags.value = 1
-#         message.author = MagicMock()
-#         del message.author.id
-#         monkeypatch.setattr(self.bot, "handle_verification", MagicMock())
+@pytest.mark.asyncio
+class TestSpellBotHandleVerification(BaseMixin):
+    async def test_missing_author_id(self, monkeypatch: pytest.MonkeyPatch):
+        message = MagicMock()
+        message.guild = MagicMock()
+        message.guild.id = 2
+        message.channel = MagicMock()
+        message.channel.type = discord.ChannelType.text
+        message.flags.value = 1
+        message.author = MagicMock()
+        del message.author.id
+        monkeypatch.setattr(self.bot, "handle_verification", MagicMock())
 
-#         await self.bot.on_message(message)
+        await self.bot.on_message(message)
 
-#         self.bot.handle_verification.assert_not_called()
+        self.bot.handle_verification.assert_not_called()
 
-#     async def test_without_auto_verify(self, dpy_message: discord.Message):
-#         assert dpy_message.guild
-#         assert dpy_message.author
-#         assert isinstance(dpy_message.guild, discord.Guild)
-#         assert isinstance(dpy_message.author, discord.User)
-#         await self.bot.handle_verification(dpy_message)
+    async def test_without_auto_verify(self, dpy_message: discord.Message):
+        assert dpy_message.guild
+        assert dpy_message.author
+        assert isinstance(dpy_message.guild, discord.Guild)
+        assert isinstance(dpy_message.author, discord.User)
+        await self.bot.handle_verification(dpy_message)
 
-#         DatabaseSession.expire_all()
-#         assert DatabaseSession.query(Guild).one().xid == dpy_message.guild.id
-#         assert DatabaseSession.query(Channel).one().xid == dpy_message.channel.id
-#         found = DatabaseSession.query(Verify).one()
-#         assert found.guild_xid == dpy_message.guild.id
-#         assert found.user_xid == dpy_message.author.id
-#         assert not found.verified
+        DatabaseSession.expire_all()
+        assert DatabaseSession.query(Guild).one().xid == dpy_message.guild.id
+        assert DatabaseSession.query(Channel).one().xid == dpy_message.channel.id
+        found = DatabaseSession.query(Verify).one()
+        assert found.guild_xid == dpy_message.guild.id
+        assert found.user_xid == dpy_message.author.id
+        assert not found.verified
 
-#     async def test_with_auto_verify(self, dpy_message: discord.Message):
-#         assert dpy_message.guild
-#         assert dpy_message.author
-#         assert isinstance(dpy_message.guild, discord.Guild)
-#         assert isinstance(dpy_message.author, discord.User)
-#         self.factories.guild.create(xid=dpy_message.guild.id)
-#         self.factories.channel.create(
-#             xid=dpy_message.channel.id,
-#             auto_verify=True,
-#             guild_xid=dpy_message.guild.id,
-#         )
+    async def test_with_auto_verify(self, dpy_message: discord.Message):
+        assert dpy_message.guild
+        assert dpy_message.author
+        assert isinstance(dpy_message.guild, discord.Guild)
+        assert isinstance(dpy_message.author, discord.User)
+        self.factories.guild.create(xid=dpy_message.guild.id)
+        self.factories.channel.create(
+            xid=dpy_message.channel.id,
+            auto_verify=True,
+            guild_xid=dpy_message.guild.id,
+        )
 
-#         await self.bot.handle_verification(dpy_message)
+        await self.bot.handle_verification(dpy_message)
 
-#         DatabaseSession.expire_all()
-#         assert DatabaseSession.query(Guild).one().xid == dpy_message.guild.id
-#         assert DatabaseSession.query(Channel).one().xid == dpy_message.channel.id
-#         found = DatabaseSession.query(Verify).one()
-#         assert found.guild_xid == dpy_message.guild.id
-#         assert found.user_xid == dpy_message.author.id
-#         assert found.verified
+        DatabaseSession.expire_all()
+        assert DatabaseSession.query(Guild).one().xid == dpy_message.guild.id
+        assert DatabaseSession.query(Channel).one().xid == dpy_message.channel.id
+        found = DatabaseSession.query(Verify).one()
+        assert found.guild_xid == dpy_message.guild.id
+        assert found.user_xid == dpy_message.author.id
+        assert found.verified
 
-#     async def test_verified_only_when_unverified(self, dpy_message: discord.Message):
-#         assert dpy_message.guild
-#         assert isinstance(dpy_message.guild, discord.Guild)
-#         self.factories.guild.create(xid=dpy_message.guild.id)
-#         self.factories.channel.create(
-#             xid=dpy_message.channel.id,
-#             verified_only=True,
-#             guild_xid=dpy_message.guild.id,
-#         )
+    async def test_verified_only_when_unverified(self, dpy_message: discord.Message):
+        assert dpy_message.guild
+        assert isinstance(dpy_message.guild, discord.Guild)
+        self.factories.guild.create(xid=dpy_message.guild.id)
+        self.factories.channel.create(
+            xid=dpy_message.channel.id,
+            verified_only=True,
+            guild_xid=dpy_message.guild.id,
+        )
 
-#         await self.bot.handle_verification(dpy_message)
+        await self.bot.handle_verification(dpy_message)
 
-#         dpy_message.delete.assert_called_once()
+        dpy_message.delete.assert_called_once()
 
-#     async def test_verified_only_when_verified(self, dpy_message: discord.Message):
-#         assert dpy_message.guild
-#         assert isinstance(dpy_message.guild, discord.Guild)
-#         assert isinstance(dpy_message.author, discord.User)
-#         self.factories.guild.create(xid=dpy_message.guild.id)
-#         self.factories.channel.create(
-#             xid=dpy_message.channel.id,
-#             verified_only=True,
-#             guild_xid=dpy_message.guild.id,
-#         )
-#         self.factories.verify.create(
-#             guild_xid=dpy_message.guild.id,
-#             user_xid=dpy_message.author.id,
-#             verified=True,
-#         )
+    async def test_verified_only_when_verified(self, dpy_message: discord.Message):
+        assert dpy_message.guild
+        assert isinstance(dpy_message.guild, discord.Guild)
+        assert isinstance(dpy_message.author, discord.User)
+        self.factories.guild.create(xid=dpy_message.guild.id)
+        self.factories.channel.create(
+            xid=dpy_message.channel.id,
+            verified_only=True,
+            guild_xid=dpy_message.guild.id,
+        )
+        self.factories.verify.create(
+            guild_xid=dpy_message.guild.id,
+            user_xid=dpy_message.author.id,
+            verified=True,
+        )
 
-#         await self.bot.handle_verification(dpy_message)
+        await self.bot.handle_verification(dpy_message)
 
-#         dpy_message.delete.assert_not_called()
+        dpy_message.delete.assert_not_called()
 
-#     async def test_unverified_only_when_unverified(self, dpy_message: discord.Message):
-#         assert dpy_message.guild
-#         assert isinstance(dpy_message.guild, discord.Guild)
-#         self.factories.guild.create(xid=dpy_message.guild.id)
-#         self.factories.channel.create(
-#             xid=dpy_message.channel.id,
-#             unverified_only=True,
-#             guild_xid=dpy_message.guild.id,
-#         )
+    async def test_unverified_only_when_unverified(self, dpy_message: discord.Message):
+        assert dpy_message.guild
+        assert isinstance(dpy_message.guild, discord.Guild)
+        self.factories.guild.create(xid=dpy_message.guild.id)
+        self.factories.channel.create(
+            xid=dpy_message.channel.id,
+            unverified_only=True,
+            guild_xid=dpy_message.guild.id,
+        )
 
-#         await self.bot.handle_verification(dpy_message)
+        await self.bot.handle_verification(dpy_message)
 
-#         dpy_message.delete.assert_not_called()
+        dpy_message.delete.assert_not_called()
 
-#     async def test_unverified_only_when_verified(self, dpy_message: discord.Message):
-#         assert dpy_message.guild
-#         assert isinstance(dpy_message.guild, discord.Guild)
-#         assert isinstance(dpy_message.author, discord.User)
-#         self.factories.guild.create(xid=dpy_message.guild.id)
-#         self.factories.channel.create(
-#             xid=dpy_message.channel.id,
-#             unverified_only=True,
-#             guild_xid=dpy_message.guild.id,
-#         )
-#         self.factories.verify.create(
-#             guild_xid=dpy_message.guild.id,
-#             user_xid=dpy_message.author.id,
-#             verified=True,
-#         )
+    async def test_unverified_only_when_verified(self, dpy_message: discord.Message):
+        assert dpy_message.guild
+        assert isinstance(dpy_message.guild, discord.Guild)
+        assert isinstance(dpy_message.author, discord.User)
+        self.factories.guild.create(xid=dpy_message.guild.id)
+        self.factories.channel.create(
+            xid=dpy_message.channel.id,
+            unverified_only=True,
+            guild_xid=dpy_message.guild.id,
+        )
+        self.factories.verify.create(
+            guild_xid=dpy_message.guild.id,
+            user_xid=dpy_message.author.id,
+            verified=True,
+        )
 
-#         await self.bot.handle_verification(dpy_message)
+        await self.bot.handle_verification(dpy_message)
 
-#         dpy_message.delete.assert_called_once()
+        dpy_message.delete.assert_called_once()
 
-#     async def test_message_from_mod_role(self, dpy_message: discord.Message, monkeypatch):
-#         assert dpy_message.guild
-#         assert isinstance(dpy_message.guild, discord.Guild)
-#         mod_role = MagicMock()
-#         mod_role.name = f"{self.settings.MOD_PREFIX}-role"
-#         monkeypatch.setattr(dpy_message.author, "roles", [mod_role])
-#         self.factories.guild.create(xid=dpy_message.guild.id)
-#         self.factories.channel.create(
-#             xid=dpy_message.channel.id,
-#             verified_only=True,
-#             guild_xid=dpy_message.guild.id,
-#         )
+    async def test_message_from_mod_role(
+        self,
+        dpy_message: discord.Message,
+        monkeypatch: pytest.MonkeyPatch,
+    ):
+        assert dpy_message.guild
+        assert isinstance(dpy_message.guild, discord.Guild)
+        mod_role = MagicMock()
+        mod_role.name = f"{self.settings.MOD_PREFIX}-role"
+        monkeypatch.setattr(dpy_message.author, "roles", [mod_role])
+        self.factories.guild.create(xid=dpy_message.guild.id)
+        self.factories.channel.create(
+            xid=dpy_message.channel.id,
+            verified_only=True,
+            guild_xid=dpy_message.guild.id,
+        )
 
-#         await self.bot.handle_verification(dpy_message)
+        await self.bot.handle_verification(dpy_message)
 
-#         dpy_message.delete.assert_not_called()
+        dpy_message.delete.assert_not_called()
 
-#     async def test_message_from_admin_role(
-#         self,
-#         dpy_message: discord.Message,
-#         monkeypatch,
-#     ):
-#         assert dpy_message.guild
-#         assert isinstance(dpy_message.guild, discord.Guild)
-#         admin_role = MagicMock()
-#         admin_role.name = self.settings.ADMIN_ROLE
-#         monkeypatch.setattr(dpy_message.author, "roles", [admin_role])
-#         self.factories.guild.create(xid=dpy_message.guild.id)
-#         self.factories.channel.create(
-#             xid=dpy_message.channel.id,
-#             verified_only=True,
-#             guild_xid=dpy_message.guild.id,
-#         )
+    async def test_message_from_admin_role(
+        self,
+        dpy_message: discord.Message,
+        monkeypatch: pytest.MonkeyPatch,
+    ):
+        assert dpy_message.guild
+        assert isinstance(dpy_message.guild, discord.Guild)
+        admin_role = MagicMock()
+        admin_role.name = self.settings.ADMIN_ROLE
+        monkeypatch.setattr(dpy_message.author, "roles", [admin_role])
+        self.factories.guild.create(xid=dpy_message.guild.id)
+        self.factories.channel.create(
+            xid=dpy_message.channel.id,
+            verified_only=True,
+            guild_xid=dpy_message.guild.id,
+        )
 
-#         await self.bot.handle_verification(dpy_message)
+        await self.bot.handle_verification(dpy_message)
 
-#         dpy_message.delete.assert_not_called()
+        dpy_message.delete.assert_not_called()
 
-#     async def test_message_from_owner(self, dpy_message: discord.Message, monkeypatch):
-#         assert dpy_message.guild
-#         assert isinstance(dpy_message.guild, discord.Guild)
-#         monkeypatch.setattr(dpy_message.author, "id", dpy_message.guild.owner_id)
-#         self.factories.guild.create(xid=dpy_message.guild.id)
-#         self.factories.channel.create(
-#             xid=dpy_message.channel.id,
-#             verified_only=True,
-#             guild_xid=dpy_message.guild.id,
-#         )
+    async def test_message_from_owner(
+        self,
+        dpy_message: discord.Message,
+        monkeypatch: pytest.MonkeyPatch,
+    ):
+        assert dpy_message.guild
+        assert isinstance(dpy_message.guild, discord.Guild)
+        monkeypatch.setattr(dpy_message.author, "id", dpy_message.guild.owner_id)
+        self.factories.guild.create(xid=dpy_message.guild.id)
+        self.factories.channel.create(
+            xid=dpy_message.channel.id,
+            verified_only=True,
+            guild_xid=dpy_message.guild.id,
+        )
 
-#         await self.bot.handle_verification(dpy_message)
+        await self.bot.handle_verification(dpy_message)
 
-#         dpy_message.delete.assert_not_called()
+        dpy_message.delete.assert_not_called()
 
-#     async def test_message_from_administrator(self, dpy_message: discord.Message):
-#         assert dpy_message.guild
-#         assert isinstance(dpy_message.guild, discord.Guild)
-#         admin_perms = discord.Permissions(discord.Permissions.administrator.flag)
-#         dpy_message.channel.permissions_for = MagicMock(return_value=admin_perms)
-#         self.factories.guild.create(xid=dpy_message.guild.id)
-#         self.factories.channel.create(
-#             xid=dpy_message.channel.id,
-#             verified_only=True,
-#             guild_xid=dpy_message.guild.id,
-#         )
+    async def test_message_from_administrator(self, dpy_message: discord.Message):
+        assert dpy_message.guild
+        assert isinstance(dpy_message.guild, discord.Guild)
+        admin_perms = discord.Permissions(discord.Permissions.administrator.flag)
+        dpy_message.channel.permissions_for = MagicMock(return_value=admin_perms)
+        self.factories.guild.create(xid=dpy_message.guild.id)
+        self.factories.channel.create(
+            xid=dpy_message.channel.id,
+            verified_only=True,
+            guild_xid=dpy_message.guild.id,
+        )
 
-#         await self.bot.handle_verification(dpy_message)
+        await self.bot.handle_verification(dpy_message)
 
-#         dpy_message.delete.assert_not_called()
+        dpy_message.delete.assert_not_called()
