3f5427e8709b22da140ced9a96a604ff28a67b42
Amy Troschinetz <amy@lexicalunit.com>
2023-04-26 13:14:18 -0700

    Refactor lfg points tests

--- src/spellbot/views/__init__.py
+++ src/spellbot/views/__init__.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
 from .base_view import BaseView
-from .lfg_view import PendingGameView, StartedGameView
+from .lfg_view import PendingGameView, StartedGameSelect, StartedGameView
 from .setup_view import SetupView
 
 __all__ = [
     "BaseView",
     "PendingGameView",
     "SetupView",
+    "StartedGameSelect",
     "StartedGameView",
 ]
