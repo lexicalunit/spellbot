0b30470203ffd837ec3140844e9e1f71e24e677c
lexicalunit <lexicalunit@lexicalunit.com>
2023-04-26 13:14:18 -0700

    Refactor lfg points tests

--- tests/cogs/test_lfg_cog_points.py
+++ tests/cogs/test_lfg_cog_points.py
@@ -1,144 +1,232 @@
 from __future__ import annotations
 
-# TODO
-# from unittest.mock import AsyncMock, MagicMock
+from typing import Callable
+from unittest.mock import MagicMock, PropertyMock, patch
 
-# import pytest
+import discord
+import pytest
+from spellbot import SpellBot
+from spellbot.actions import lfg_action
+from spellbot.cogs import LookingForGameCog
+from spellbot.database import DatabaseSession
+from spellbot.models import Channel, Game, Play
+from spellbot.settings import Settings
+from spellbot.views import StartedGameSelect, StartedGameView
 
-# from spellbot import SpellBot
-# from spellbot.cogs import LookingForGameCog
-# from spellbot.database import DatabaseSession
-# from spellbot.actions import lfg_action
-# from spellbot.models import GameStatus, Play
-# from spellbot.settings import Settings
-# from tests.fixtures import Factories
-# from tests.mocks import mock_discord_user, mock_operations
+from tests.mixins import InteractionMixin
+from tests.mocks import mock_discord_object, mock_operations
 
 
-# @pytest.mark.asyncio()
-# class TestCogLookingForGamePoints:
-#     async def test_points(
-#         self,
-#         bot: SpellBot,
-#         ctx: ComponentContext,
-#         settings: Settings,
-#         factories: Factories,
-#     ):
-#         guild = factories.guild.create(xid=ctx.guild_id, show_points=True)
-#         channel = factories.channel.create(xid=ctx.channel_id, guild=guild)
-#         game = factories.game.create(
-#             guild=guild,
-#             channel=channel,
-#             seats=2,
-#             status=GameStatus.STARTED.value,
-#             message_xid=12345,
-#         )
-#         user1 = factories.user.create(xid=ctx.author_id, game=game)
-#         user2 = factories.user.create(game=game)
-#         factories.play.create(user_xid=user1.xid, game_id=game.id, points=0)
-#         factories.play.create(user_xid=user2.xid, game_id=game.id, points=0)
+@pytest.fixture()
+def cog(bot: SpellBot) -> LookingForGameCog:
+    return LookingForGameCog(bot)
 
-#         message = MagicMock()
-#         message.id = game.message_xid
-#         message.edit = AsyncMock()
 
-#         ctx.selected_options = [5]
-#         ctx.defer = AsyncMock()
-#         ctx.origin_message = message
-#         cog = LookingForGameCog(bot)
-#         await cog.points.func(cog, ctx)
+@pytest.mark.asyncio()
+class TestCogLookingForGamePoints(InteractionMixin):
+    async def test_points(
+        self,
+        cog: LookingForGameCog,
+        settings: Settings,
+        add_channel: Callable[..., Channel],
+    ) -> None:
+        channel = add_channel(default_seats=2, xid=self.interaction.channel_id, show_points=True)
+        game = self.factories.game.create(
+            guild=self.guild,
+            channel=channel,
+            seats=2,
+            message_xid=123,
+        )
+        user = self.factories.user.create(xid=self.interaction.user.id)
+        other_user = self.factories.user.create(xid=self.interaction.user.id + 1, game=game)
+        other_player = mock_discord_object(other_user)
+        message = MagicMock(spec=discord.Message, id=game.message_xid)
+        self.interaction.original_response.return_value = message
 
-#         found = DatabaseSession.query(Play).filter(Play.user_xid == user1.xid).one()
-#         assert found.points == 5
-#         assert message.edit.call_args_list[0].kwargs["embed"].to_dict() == {
-#             "color": settings.EMBED_COLOR,
-#             "description": (
-#                 "Please check your Direct Messages for your SpellTable link.\n\n"
-#                 "When your game is over use the drop down to report your points.\n\n"
-#                 f"{guild.motd}\n\n{channel.motd}"
-#             ),
-#             "fields": [
-#                 {
-#                     "inline": False,
-#                     "name": "Players",
-#                     "value": f"<@{user1.xid}> (5 points), <@{user2.xid}>",
-#                 },
-#                 {"inline": True, "name": "Format", "value": "Commander"},
-#             ],
-#             "footer": {"text": f"SpellBot Game ID: #SB{game.id}"},
-#             "thumbnail": {"url": settings.THUMB_URL},
-#             "title": "**Your game is ready!**",
-#             "type": "rich",
-#         }
+        with patch.object(
+            StartedGameSelect,
+            "values",
+            new_callable=PropertyMock,
+        ) as values:
+            view = StartedGameView(bot=self.bot)
+            select = view._children[0]
+            values.return_value = ["5"]
+            with mock_operations(lfg_action, users=[other_player]):
+                lfg_action.safe_get_partial_message.return_value = message
 
-#     async def test_points_when_message_not_found(
-#         self,
-#         bot: SpellBot,
-#         ctx: ComponentContext,
-#         factories: Factories,
-#     ):
-#         guild = factories.guild.create(xid=ctx.guild_id, show_points=True)
-#         channel = factories.channel.create(xid=ctx.channel_id, guild=guild)
-#         game = factories.game.create(
-#             guild=guild,
-#             channel=channel,
-#             seats=2,
-#             status=GameStatus.STARTED.value,
-#             message_xid=12345,
-#         )
-#         user1 = factories.user.create(xid=ctx.author_id, game=game)
-#         user2 = factories.user.create(game=game)
-#         factories.play.create(user_xid=user1.xid, game_id=game.id, points=0)
-#         factories.play.create(user_xid=user2.xid, game_id=game.id, points=0)
+                await self.run(cog.lfg)
 
-#         message = MagicMock()
-#         message.id = game.message_xid + 10  # +10 so that it won't be found
-#         message.edit = AsyncMock()
+                DatabaseSession.expire_all()
+                game = DatabaseSession.query(Game).one()
+                assert lfg_action.safe_update_embed.call_args_list[0].kwargs["embed"].to_dict() == {
+                    "color": self.settings.EMBED_COLOR,
+                    "description": (
+                        "Please check your Direct Messages for your SpellTable link.\n\n"
+                        "When your game is over use the drop down to report your points.\n\n"
+                        f"{self.guild.motd}\n\n{channel.motd}"
+                    ),
+                    "fields": [
+                        {
+                            "inline": False,
+                            "name": "Players",
+                            "value": f"<@{self.interaction.user.id}>, <@{other_player.id}>",
+                        },
+                        {"inline": True, "name": "Format", "value": "Commander"},
+                        {
+                            "inline": True,
+                            "name": "Started at",
+                            "value": f"<t:{game.started_at_timestamp}>",
+                        },
+                    ],
+                    "footer": {"text": f"SpellBot Game ID: #SB{game.id}"},
+                    "thumbnail": {"url": self.settings.THUMB_URL},
+                    "title": "**Your game is ready!**",
+                    "type": "rich",
+                }
 
-#         ctx.selected_options = [5]
-#         ctx.defer = AsyncMock()
-#         ctx.origin_message = message
-#         cog = LookingForGameCog(bot)
-#         await cog.points.func(cog, ctx)
+                await select.callback(self.interaction)
 
-#         found = DatabaseSession.query(Play).filter(Play.user_xid == user1.xid).one()
-#         assert found.points == 0  # hasn't changed
+                found = DatabaseSession.query(Play).filter(Play.user_xid == user.xid).one()
+                assert found.points == 5
 
-#     async def test_points_when_not_in_game(
-#         self,
-#         bot: SpellBot,
-#         ctx: ComponentContext,
-#         factories: Factories,
-#     ):
-#         guild = factories.guild.create(xid=ctx.guild_id, show_points=True)
-#         channel = factories.channel.create(xid=ctx.channel_id, guild=guild)
-#         game = factories.game.create(
-#             guild=guild,
-#             channel=channel,
-#             seats=2,
-#             status=GameStatus.STARTED.value,
-#             message_xid=12345,
-#         )
-#         user1 = factories.user.create(xid=ctx.author_id)
-#         user2 = factories.user.create(game=game)
-#         factories.play.create(user_xid=user2.xid, game_id=game.id, points=0)
+                assert lfg_action.safe_update_embed.call_args_list[1].kwargs["embed"].to_dict() == {
+                    "color": settings.EMBED_COLOR,
+                    "description": (
+                        "Please check your Direct Messages for your SpellTable link.\n\n"
+                        "When your game is over use the drop down to report your points.\n\n"
+                        f"{self.guild.motd}\n\n{channel.motd}"
+                    ),
+                    "fields": [
+                        {
+                            "inline": False,
+                            "name": "Players",
+                            "value": f"<@{user.xid}> (5 points), <@{other_user.xid}>",
+                        },
+                        {"inline": True, "name": "Format", "value": "Commander"},
+                        {
+                            "inline": True,
+                            "name": "Started at",
+                            "value": f"<t:{game.started_at_timestamp}>",
+                        },
+                    ],
+                    "footer": {"text": f"SpellBot Game ID: #SB{game.id}"},
+                    "thumbnail": {"url": settings.THUMB_URL},
+                    "title": "**Your game is ready!**",
+                    "type": "rich",
+                }
 
-#         message = MagicMock()
-#         message.id = game.message_xid
-#         message.edit = AsyncMock()
+    async def test_points_when_message_not_found(
+        self,
+        cog: LookingForGameCog,
+        settings: Settings,
+        add_channel: Callable[..., Channel],
+    ) -> None:
+        channel = add_channel(default_seats=2, xid=self.interaction.channel_id, show_points=True)
+        game = self.factories.game.create(
+            guild=self.guild,
+            channel=channel,
+            seats=2,
+            message_xid=123,
+        )
+        user = self.factories.user.create(xid=self.interaction.user.id)
+        other_user = self.factories.user.create(xid=self.interaction.user.id + 1, game=game)
+        other_player = mock_discord_object(other_user)
+        message = MagicMock(spec=discord.Message, id=game.message_xid)
+        self.interaction.original_response.return_value = message
 
-#         ctx.selected_options = [5]
-#         ctx.defer = AsyncMock()
-#         ctx.origin_message = message
+        with patch.object(
+            StartedGameSelect,
+            "values",
+            new_callable=PropertyMock,
+        ) as values:
+            view = StartedGameView(bot=self.bot)
+            select = view._children[0]
+            values.return_value = ["5"]
+            with mock_operations(lfg_action, users=[other_player]):
+                lfg_action.safe_get_partial_message.return_value = message
 
-#         player1 = mock_discord_user(user1)
-#         player2 = mock_discord_user(user2)
+                await self.run(cog.lfg)
 
-#         with mock_operations(lfg_action, users=[player1, player2]):
-#             cog = LookingForGameCog(bot)
-#             await cog.points.func(cog, ctx)
+                DatabaseSession.expire_all()
+                game = DatabaseSession.query(Game).one()
+                assert lfg_action.safe_update_embed.call_args_list[0].kwargs["embed"].to_dict() == {
+                    "color": self.settings.EMBED_COLOR,
+                    "description": (
+                        "Please check your Direct Messages for your SpellTable link.\n\n"
+                        "When your game is over use the drop down to report your points.\n\n"
+                        f"{self.guild.motd}\n\n{channel.motd}"
+                    ),
+                    "fields": [
+                        {
+                            "inline": False,
+                            "name": "Players",
+                            "value": f"<@{self.interaction.user.id}>, <@{other_player.id}>",
+                        },
+                        {"inline": True, "name": "Format", "value": "Commander"},
+                        {
+                            "inline": True,
+                            "name": "Started at",
+                            "value": f"<t:{game.started_at_timestamp}>",
+                        },
+                    ],
+                    "footer": {"text": f"SpellBot Game ID: #SB{game.id}"},
+                    "thumbnail": {"url": self.settings.THUMB_URL},
+                    "title": "**Your game is ready!**",
+                    "type": "rich",
+                }
 
-#             lfg_action.safe_send_user.assert_called_once_with(
-#                 ctx.author,
-#                 "You are not one of the players in this game.",
-#             )
+                message.id += 1  # make it so the message isn't findable in our DB
+                lfg_action.safe_get_partial_message.return_value = message
+                self.interaction.original_response.return_value = message
+                lfg_action.safe_update_embed.reset_mock()
+
+                await select.callback(self.interaction)
+
+                found = DatabaseSession.query(Play).filter(Play.user_xid == user.xid).one()
+                assert found.points is None
+                lfg_action.safe_update_embed.assert_not_called()
+
+    async def test_points_when_not_in_game(
+        self,
+        cog: LookingForGameCog,
+        settings: Settings,
+        add_channel: Callable[..., Channel],
+    ) -> None:
+        channel = add_channel(default_seats=2, xid=self.interaction.channel_id, show_points=True)
+        game = self.factories.game.create(
+            guild=self.guild,
+            channel=channel,
+            seats=2,
+            message_xid=123,
+        )
+        self.factories.user.create(xid=self.interaction.user.id)
+        other_user = self.factories.user.create(xid=self.interaction.user.id + 1, game=game)
+        other_player = mock_discord_object(other_user)
+        message = MagicMock(spec=discord.Message, id=game.message_xid)
+        self.interaction.original_response.return_value = message
+
+        outside_user = self.factories.user.create(xid=self.interaction.user.id + 2)
+        outside_player = mock_discord_object(outside_user)
+
+        with patch.object(
+            StartedGameSelect,
+            "values",
+            new_callable=PropertyMock,
+        ) as values:
+            view = StartedGameView(bot=self.bot)
+            select = view._children[0]
+            values.return_value = ["5"]
+            with mock_operations(lfg_action, users=[other_player, outside_player]):
+                lfg_action.safe_get_partial_message.return_value = message
+
+                await self.run(cog.lfg)
+
+                # change the interaction user to a user not in the game
+                self.interaction.user = outside_player
+                lfg_action.safe_send_user.reset_mock()
+                await select.callback(self.interaction)
+
+                lfg_action.safe_send_user.assert_called_once_with(
+                    outside_player,
+                    "You are not one of the players in this game.",
+                )
