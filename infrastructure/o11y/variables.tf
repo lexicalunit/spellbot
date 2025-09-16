06e1d82df6d27ba7d76538468b01cdbc0516eafe
lexicalunit <lexicalunit@lexicalunit.com>
2025-09-16 15:23:52 -0700

    Adds dd monitor tf config

---
+++ infrastructure/o11y/variables.tf
@@ -0,0 +1,16 @@
+variable "datadog_api_key" {
+  description = "Datadog API key"
+  type        = string
+  sensitive   = true
+}
+
+variable "datadog_app_key" {
+  description = "Datadog Application key"
+  type        = string
+  sensitive   = true
+}
+
+variable "alert_email" {
+  description = "Email to send alerts to"
+  type        = string
+}
