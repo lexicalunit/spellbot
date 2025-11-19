c094dcc5d726da6cc23df703a40c8acf8c66050b
lexicalunit <lexicalunit@lexicalunit.com>
2025-11-18 18:01:18 -0800

    Fix bug with convoke password

--- src/spellbot/integrations/convoke.py
+++ src/spellbot/integrations/convoke.py
@@ -144,7 +144,7 @@ async def generate_link(game: GameDict) -> tuple[str | None, str | None]:  # pra
             if not data:
                 return None, None
             game_id = data["id"]
-            game_pass = data["password"]
+            game_pass = data.get("password") or key
             return f"https://www.convoke.games/en/play/private/{game_id}", game_pass
 
     return None, None
