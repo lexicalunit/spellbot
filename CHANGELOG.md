cba32db8566aa6fcc0ed361f40be66f43b10de2f
Amy Troschinetz <amy@lexicalunit.com>
2025-12-06 14:25:35 -0800

    Adds scopes to API tokens

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Added
+
+- Adds scopes to API tokens.
+
 ## [v17.2.2](https://github.com/lexicalunit/spellbot/releases/tag/v17.2.2) - 2025-12-06
 
 ### Added
--- src/spellbot/services/apps.py
+++ src/spellbot/services/apps.py
@@ -10,5 +10,16 @@ from spellbot.models import Token
 class AppsService:
     @sync_to_async()
     @tracer.wrap()
-    def verify_token(self, key: str) -> bool:
-        return bool(DatabaseSession.query(Token).filter(Token.key == key).count())
+    def verify_token(self, key: str, path: str) -> bool:
+        token = DatabaseSession.query(Token).filter(Token.key == key).one_or_none()
+        if not token:
+            return False
+        if token.deleted_at:
+            return False
+        if token.scopes == "*":
+            return True
+        required_scope = path.lstrip("/").split("/")[1]
+        if not required_scope:
+            return False
+        scopes_list = token.scopes.split(",")
+        return required_scope in scopes_list
