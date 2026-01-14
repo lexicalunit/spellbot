3d05604f7f91a5b98fcb723d5c6f37cede1f2ce5
lexicalunit <lexicalunit@lexicalunit.com>
2026-01-14 14:18:02 -0800

    Fix issue with supporter emoji not always showing up

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Fixed
+
+- Fixed issues with supporter emojis not always showing up.
+
 ## [v17.7.0](https://github.com/lexicalunit/spellbot/releases/tag/v17.7.0) - 2026-01-13
 
 ### Added
--- src/spellbot/models/game.py
+++ src/spellbot/models/game.py
@@ -399,7 +399,7 @@ class Game(Base):
         def emoji(xid: int) -> str:
             if supporter_emoji and xid in supporters:
                 return f"{supporter_emoji} "
-            if xid == 711717544435646494:  # test account
+            if supporter_emoji and xid == 711717544435646494:  # test account
                 return f"{supporter_emoji} "
             if owner_emoji and settings.OWNER_XID and xid == int(settings.OWNER_XID):
                 return f"{owner_emoji} "
