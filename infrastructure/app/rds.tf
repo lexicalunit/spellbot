e01496a07bbbc5ade9390b2f6b441cb3b2a29f6c
lexicalunit <lexicalunit@lexicalunit.com>
2026-05-20 19:18:32 -0700

    fix validation issues

--- infrastructure/app/rds.tf
+++ infrastructure/app/rds.tf
@@ -7,18 +7,22 @@ module "aurora_cluster" {
   engine_version              = "17.5"
   master_username             = "postgres"
   manage_master_user_password = true
-  publicly_accessible         = true
+
   # Use the smallest Aurora instance type
   instances = {
     one = {
-      instance_class = "db.t4g.medium"
+      instance_class                        = "db.t4g.medium"
+      publicly_accessible                   = true
+      monitoring_interval                   = 60
+      performance_insights_enabled          = true
+      performance_insights_retention_period = 7
     }
   }
 
   # Use database subnets from VPC module
   vpc_id               = module.vpc.vpc_id
   db_subnet_group_name = module.vpc.database_subnet_group_name
-  security_group_rules = {
+  security_group_ingress_rules = {
     database_ingress = {
       source_security_group_id = aws_security_group.ecs_tasks.id
     }
@@ -29,11 +33,5 @@ module "aurora_cluster" {
   preferred_backup_window      = "03:00-06:00"
   preferred_maintenance_window = "Mon:00:00-Mon:03:00"
 
-  # Monitoring
-  monitoring_interval = 60
-
   deletion_protection = true
-
-  # Performance Insights
-  performance_insights_enabled = true
 }
